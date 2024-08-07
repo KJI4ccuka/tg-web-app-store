b1b8e712b86218c91f72f9cf36d029f3fc46a2bc
David <grikurovdavid@gmail.com>
2024-08-07 21:03:15 +0200

    update

--- src/components/product-item/product-item.jsx
+++ src/components/product-item/product-item.jsx
@@ -9,14 +9,14 @@ const ProductItem = ({product, className, onAdd}) => {
     }
 
     return (
-        <div className={cl.product + className}>
+        <div className={cl.product}>
             <div className={cl.img}/>
             <div className={cl.title}>{product.title}</div>
             <div className={cl.description}>{product.description}</div>
             <div className={cl.price}>
                 <span>Стоимость: <b>{product.price}</b></span>
             </div>
-            <Button className={cl.add-btn} onClick={onAddHandler}>
+            <Button className={cl.addBtn} onClick={onAddHandler}>
                 Добавить в корзину
             </Button>
         </div>
