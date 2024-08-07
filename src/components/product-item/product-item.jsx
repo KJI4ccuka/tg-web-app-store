12d5a01a73c81cd1553195508b0657f40e050c70
David <grikurovdavid@gmail.com>
2024-08-07 20:17:31 +0200

    update

---
+++ src/components/product-item/product-item.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import cl from  './product-item.module.scss';
+import Button from "../button/button.jsx";
+
+const ProductItem = ({product, className, onAdd}) => {
+
+    const onAddHandler = () => {
+        onAdd(product);
+    }
+
+    return (
+        <div className={'product ' + className}>
+            <div className={cl.img}/>
+            <div className={cl.title}>{product.title}</div>
+            <div className={cl.description}>{product.description}</div>
+            <div className={cl.price}>
+                <span>Стоимость: <b>{product.price}</b></span>
+            </div>
+            <Button className={'add-btn'} onClick={onAddHandler}>
+                Добавить в корзину
+            </Button>
+        </div>
+    );
+};
+
+export default ProductItem;
